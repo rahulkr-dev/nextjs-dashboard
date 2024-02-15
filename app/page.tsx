c889831fa0427679112b6b724135437f3bd002a6
rahul <rkrahul8181@gmail.com>
2024-02-15 21:09:11 +0530

    add active link for navigation menu

--- app/page.tsx
+++ app/page.tsx
@@ -29,12 +29,12 @@ export default function Page() {
           {/* Add Hero Images Here */}
 
           <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
